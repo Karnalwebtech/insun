5f7567f8243a3125cb8738946cf68067b7139004
pawan <pawan.irish@gmail.com>
2025-02-16 21:15:12 +0530

    dfsdf

--- server/src/utils/uuidGenrator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const uuid_1 = require("uuid");
-function uuidGenerator() {
-    return (0, uuid_1.v4)().slice(0, 8); // ✅ Corrected function call and slicing
-}
-exports.default = uuidGenerator;
