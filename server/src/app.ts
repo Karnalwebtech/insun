1db157f9224c2aa7c75329980d9571f3b9d1bb32
pawan <pawan.irish@gmail.com>
2025-02-17 21:17:29 +0530

    sdsdsd

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://insun.vercel.app/"],
+    origin: ["http://localhost:3000","https://insun.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
